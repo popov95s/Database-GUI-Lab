c7833582b5a0e3bde8e412281705399d66f352fb
Gabo <gleyvamerino@gmail.com>
2016-10-25 11:44:09 -0500

    Track Location Added
    Gets current location

--- frontend/Parkit/src/app/app.module.ts
+++ frontend/Parkit/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { LocationTracker } from '../location/location-tracker';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { TabsPage } from '../pages/tabs/tabs';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -23,8 +24,10 @@ import { TabsPage } from '../pages/tabs/tabs';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+
+
   ],
-  providers: []
+  providers: [LocationTracker]
 })
 export class AppModule {}
