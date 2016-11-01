f2f824c1e5f2750b4e27b1631bb0bbf496056155
Gabo <gleyvamerino@gmail.com>
2016-11-01 12:10:18 -0500

    added map
    Embedded Google Maps into the application

--- frontend/Parkit/src/pages/about/about.ts
+++ frontend/Parkit/src/pages/about/about.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+import { Geolocation } from 'ionic-native';
+import { GoogleMap, GoogleMapsEvent, GoogleMapsLatLng } from 'ionic-native';
+
+// ios API KEY: AIzaSyAPJVrypTj0ZaAd-xO8egPEyiUpmnt2QZs
+
 
 @Component({
   selector: 'page-about',
@@ -7,8 +12,44 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  map: GoogleMap;
 
-  }
+   constructor(public navCtrl: NavController, public platform: Platform) {
+       platform.ready().then(() => {
+           this.loadMap();
+       });
+   }
+
+   loadMap(){
+
+       let location = new GoogleMapsLatLng(-34.9290,138.6010);
+
+       this.map = new GoogleMap('map', {
+         'backgroundColor': 'white',
+         'controls': {
+           'compass': true,
+           'myLocationButton': true,
+           'indoorPicker': true,
+           'zoom': true
+         },
+         'gestures': {
+           'scroll': true,
+           'tilt': true,
+           'rotate': true,
+           'zoom': true
+         },
+         'camera': {
+           'latLng': location,
+           'tilt': 30,
+           'zoom': 15,
+           'bearing': 50
+         }
+       });
+
+       this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
+           console.log('Map is ready!');
+       });
+
+   }
 
 }
