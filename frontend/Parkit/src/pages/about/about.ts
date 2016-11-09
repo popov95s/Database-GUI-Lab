57a34169ff2ee8d323d2298d4b9a54fc37747feb
Gabo <gleyvamerino@gmail.com>
2016-11-09 17:13:45 -0600

    lol map works like 50%

--- frontend/Parkit/src/pages/about/about.ts
+++ frontend/Parkit/src/pages/about/about.ts
@@ -1,18 +1,16 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController, Platform } from 'ionic-angular';
 import { Geolocation } from 'ionic-native';
-import { GoogleMap, GoogleMapsEvent, GoogleMapsLatLng } from 'ionic-native';
+import { GoogleMap, GoogleMapsEvent, GoogleMapsLatLng, GoogleMapsMarkerOptions, GoogleMapsMarker, CameraPosition } from 'ionic-native';
 
 // ios API KEY: AIzaSyAPJVrypTj0ZaAd-xO8egPEyiUpmnt2QZs
-
-
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
 
-  map: GoogleMap;
+ map: GoogleMap;
 
    constructor(public navCtrl: NavController, public platform: Platform) {
        platform.ready().then(() => {
@@ -20,12 +18,29 @@ export class AboutPage {
        });
    }
 
+
+   addMarkers(){
+     // create new marker
+      let marker = new GoogleMapsMarker({
+     position: location,
+     draggable: true,
+     map: this.map,
+     })
+
+     marker.setVisible(true);
+
+     this.map.addMarker(marker).then((marker1: GoogleMapsMarker) => {
+       marker1.showInfoWindow();
+     });
+
+   }
+
    loadMap(){
 
-       let location = new GoogleMapsLatLng(-34.9290,138.6010);
+       let location1 = new GoogleMapsLatLng(-34.9290,138.6010);
 
        this.map = new GoogleMap('map', {
-         'backgroundColor': 'white',
+         'backgroundColor': 'nil',
          'controls': {
            'compass': true,
            'myLocationButton': true,
@@ -39,17 +54,35 @@ export class AboutPage {
            'zoom': true
          },
          'camera': {
-           'latLng': location,
+           'latLng': location1,
            'tilt': 30,
            'zoom': 15,
            'bearing': 50
          }
        });
 
+
        this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
            console.log('Map is ready!');
+           // create new marker
+           // create LatLng object
+           let location: GoogleMapsLatLng = new GoogleMapsLatLng(43.0741904,-89.3809802);
+
+          //  let marker = new GoogleMapsMarker({
+          // position: location,
+          // draggable: true,
+          // map: this.map,
+          // })
+          //
+          // marker.setVisible(true);
+          //
+          // this.map.addMarker(marker).then((marker1: GoogleMapsMarker) => {
+          //   marker1.showInfoWindow();
+          // });
+
+
        });
 
    }
 
-}
+ }
