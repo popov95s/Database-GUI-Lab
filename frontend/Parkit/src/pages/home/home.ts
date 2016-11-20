52f7b3ebda4848a503fa28abbd710f114b4a5685
popov95s <spopov@smu.edu>
2016-11-20 15:44:58 -0600

    Fixed HomeView

--- frontend/Parkit/src/pages/home/home.ts
+++ frontend/Parkit/src/pages/home/home.ts
@@ -1,9 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, Input } from '@angular/core';
 import { MenuController } from 'ionic-angular';
 import { NavController } from 'ionic-angular';
 import { LocationTracker } from '../../location/location-tracker';
 import { ChartComponent } from '../charts/charts.component';
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -12,11 +11,23 @@ import { ChartComponent } from '../charts/charts.component';
 })
 
 export class HomePage {
-
+  parkingLots=[ {parkingLot: "Binkley"}, 
+                {parkingLot:"Theta Lot"},
+                {parkingLot:"Moody"},
+                {parkingLot: "Airline"}
+  ];
+  @Input() currentParkingLot:String;
   constructor(public navCtrl: NavController, public menuCtrl: MenuController, public locationTracker: LocationTracker) {
-
-  }
-
+      //this.parkingLots.push("Binkley");
+      // this.parkingLots.push({parkingLot:"Airline"});
+      // this.parkingLots.push({parkingLot:"Theta Lot"});
+      // this.parkingLots.push({parkingLot:"Moody"});
+      this.currentParkingLot="Moody";
+  } 
+changeName(parkingLot:String){
+  this.currentParkingLot=parkingLot;
+  console.log(this.currentParkingLot);
+}
 openMenu() {
   this.menuCtrl.open();
 }
