0e8b4e72b631d5349ba941771ad9bd63abb34e68
Adrien Ibarra <aibarra@smu.edu>
2016-11-27 13:23:16 -0600

    Added ParkingInfo table for checkin, as well as routes for checkin, signup, passwords, and checkout

--- app/__init__.py
+++ app/__init__.py
@@ -1,28 +1,118 @@
-from flask import Flask, g, jsonify
+from flask import Flask, g, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_httpauth import HTTPBasicAuth
-from .errors import forbidden, unauthorized
+from .errors import bad_request, forbidden, unauthorized
+import os
+
+if os.path.exists('../parkit.env'):
+    print "Importing environment from parkit.env..."
+    for line in open('../parkit.env'):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1]
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + \
+    os.getenv('MYSQL_USER') + ':' + os.getenv('MYSQL_PASS') + '@' + 'localhost/Parkit'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = \
+        r'ac7\x14\xb5L9\x8b\xae<\xd3\xc3\xfe\xa9\x15\x9c\xf9\xd3\xdf\x10\x1b\xc9'
+
 auth = HTTPBasicAuth()
 
 db = SQLAlchemy(app)
 
-from app import models
+from .models import User, Lot
 
 @app.route('/')
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    return "Parkit API"
 
+# TODO: add try/except blocks for the username/password retrieval from JSON
 @app.route('/login', methods=['GET','POST'])
 def login():
-    return "Success!"
+    login_info = request.get_json()
+    try:
+        if login_info['username'] is None or login_info['password'] is None:
+            return bad_request('No username or password provided.')
+    except:
+        return bad_request('JSON was unable to be parsed.')
+    user = User.query.filter_by(username=login_info['username']).first()
+    if user is not None and user.verify_password(login_info['password']):
+        g.current_user = user
+        return jsonify({"Authorization": user.generate_auth_token()})
+    return unauthorized('Incorrect username or password.')
+
+# TODO: find a much better way to check if values are none
+@app.route('/signup', methods=['GET', 'POST]')
+def signup():
+    sign_up_info = request.get_json()
+    try:
+        if sign_up_info['username'] is None or sign_up_info['password'] is None or sign_up_info['email'] is None or sign_up_info['first_name'] is None or sign_up_info['last_name'] is None or sign_up_info['parkitLot'] is None:
+            return bad_request('No username or password provided')
+    except:
+        return bad_request('JSON was unable to be parsed')
+    if(User.query.filter_by(username=sign_up_info['username']).first() is not None):
+        return bad_request("Username is already registered")
+    else:    
+        user = User(username = sign_up_info['username'],
+                    password = sign_up_info['password'],
+                    email = sign_up_info['email'],
+                    first_name = sign_up_info['first_name'],
+                    last_name = sign_up_info['last_name'],
+                    parkingLot = sign_up_info['parkingLot'])
+        db.session.add(user)
+        g.current_user = user
+        return jsonify({"Authorization": user.generate_auth_token()})
+
+# TODO: figure out sending emails for resetting passwords
+@app.route('/forgotpass', methods=['GET', 'POST'])
+def forgotpass:
+    email_info = request.get_json()
+    try:
+        if email_info['email'] is None:
+            return bad_request('No email provided')
+    except:
+        return bad_request('JSON was unable to be parsed')
+    user = User.query.filter_by(email = email_info['email'])
+    if user is not None:
+        """email stuff"""
+
+# TODO: Coordinate what methods return what errors
+@app.route('/checkin', methods=['GET', 'POST'])
+def checkin():
+    auth_token = request.headers.get('Authorization')
+    checkin_info = request.get_json()
+    user = verify_auth_token(auth_token)
+    try:
+        if user is not None:
+            lot_parked = checkin_info['parkingLot']
+            floor_parked = checkin_info['floor']
+            new_checkin = ParkingInfo(username = user, Lot = lot_parked, floor = floor_parked)
+            db.session.add(new_checkin)
+    except:
+        return bad_request("something idk could be unauthorized")
+    return unauthorized("idk some error")
+
+# TODO: Not sure if this is the proper/safe way to delete user, figure out how to get rid of auth token, not sure what to return (apiary sucks)
+@app.route('/checkout', methods=['DELETE'])
+def checkout:
+    checkout_user_token = request.headers.get('Authorization')
+    user = verify_auth_token(checkout_user_token)
+    try:
+        if user is not None:
+            db.session.delete(user)
+            return jsonify({})
+    except:
+        return bad_request("Unable to delete")
+    return unauthorized("You can't delete this user")
 
 @auth.verify_password
 def verify_password(username_or_token, password):
     if username_or_token == '':
         return False
-    if password == '':
+    if password == '' or password is None:
         g.current_user = User.verify_auth_token(username_or_token)
         g.token_used = True
         return g.current_user is not None
@@ -34,13 +124,13 @@ def verify_password(username_or_token, password):
 
 @auth.error_handler
 def auth_error():
-    return unauthorized('Invalid credentials')
+    return unauthorized('Invalid credentials.')
 
 @app.route('/token', methods=['GET'])
 @auth.login_required
 def get_token():
     if g.token_used:
-        return unauthorized('Invalid credentials')
+        return unauthorized('Invalid credentials.')
     return jsonify({"Authorization": g.current_user.generate_auth_token()})
 
 if __name__ == "__main__":
--- app/models.py
+++ app/models.py
@@ -11,46 +11,50 @@ class Lot(db.Model):
     spots = db.Column(db.Integer, nullable=False)
     spots_taken = db.Column(db.Integer, nullable=False)
 
-    def __init__(self, lot_name, latitude, longitude, spots, spots_taken):
-        self.lot_name = lot_name
-        self.latitude = latitude
-        self.longitude = longitude
-        self.spots = spots
-        self.spots_taken = spots_taken
-    
+    def __init__(self, **kwargs):
+        super(Lot, self).__init__(**kwargs)
+
+    def __repr__(self):
+        return '<Lot %r>' % self.lot_name
+
 class User(db.Model):
     __tablename__ = 'Users'
     user_id = db.Column(db.Integer, primary_key=True, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), unique=True, nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
     first_name = db.Column(db.String(80), nullable=False)
     last_name = db.Column(db.String(80), nullable=False) 
     favorite_lot = db.Column(db.String(80))
     confirmed = db.Column(db.Boolean, default=False)
+    parking_info = db.relationship('ParkingInfo')
+    
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
 
-    def __init__(self, user_id, username, password_hash, email, first_name,
-            last_name, favorite_lot):
-        self.user_id = user_id
-        self.username = username
-        self.password_hash = generate_password_hash(password_hash)
-        self.email = email
-        self.first_name = first_name
-        self.last_name = last_name
-        self.favorite_lot = favorite_lot
+class ParkingInfo(db.Model):
+    __tablename__ = "ParkingInfo"
+    parking_id = db.Column(db.Integer, primary_key=True, autoincrement=True,  nullable=False)
+    parking_user_id = db.Column(db.Integer, db.ForeignKey('user_id'))
+    lot = db.Column(db.String(80), nullable=False)
+    floor = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, parking_id, parking_user_id, lot, floor):
+	self.parking_id = parking_id
+	self.parking_user_id = parking_user_id
+	self.lot = lot
+	self.floor = floor
 
     def generate_auth_token(self, expiration=3600):
         s = Serializer(current_app.config['SECRET_KEY'], expires_in=expiration)
         return s.dumps({"Authorization": self.user_id})
 
     @staticmethod
-    def verify_auth_token(self, token):
+    def verify_auth_token(token):
         s = Serializer(current_app.config['SECRET_KEY'])
         try:
             data = s.loads(token)
         except:
-            return None
-        if data['Authorization'] != self.user_id:
             return None
         return User.query.get(data['Authorization'])
 
@@ -58,5 +62,12 @@ class User(db.Model):
     def password(self):
         raise AttributeError('Password is not a readable attribute.')
 
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
